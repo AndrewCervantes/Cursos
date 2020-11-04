02b14c706c573e38e7e619907b48478cb10b4b69
AndrewCervantes <andrez.cris10@gmail.com>
2020-11-03 22:14:48 -0600

    Ejercicios finales de funciones

--- Python/Funciones/Ejercicios.py
+++ Python/Funciones/Ejercicios.py
@@ -24,3 +24,38 @@ def relacion(a,b):
 print("Primer intento", relacion(5,10))
 print("Segundo intento", relacion(10,5))
 print("Tercero intento", relacion(5,5))
+
+
+#Ejercicio 4
+def intermedio(a,b):
+    return (a+b)/2
+
+print("Intermedio: ", intermedio(-12,24))
+
+
+#Ejercicio 5
+def recortar(numero, minimo, maximo):
+    if numero < minimo:
+        return minimo
+    elif numero > maximo:
+        return maximo
+    else:
+        numero
+
+print("Recortar: ", recortar(15,0,10))
+
+#Ejercicio 6
+def separar(lista):
+    lista.sort()
+    pares = []
+    impares = []
+    for numeros in lista:
+        if numeros%2 == 0:
+            pares.append(numeros)
+        else:
+            impares.append(numeros)
+    return pares, impares
+
+pares, impares = separar([-12, 84, 13, 20, -33, 101, 9])
+print("Lista de pares: ", pares)
+print("Lista de impares: ",impares)
