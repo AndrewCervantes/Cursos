2f55357085823e02bfc801fdbc97300ee3ddc9b7
AndrewCervantes <andrez.cris10@gmail.com>
2020-11-23 22:56:51 -0600

    Superclase

--- Python/Herencia/Herencia.py
+++ Python/Herencia/Herencia.py
@@ -1,19 +1,69 @@
 # Herencia
 #Capacidad de una clase de heredar atrributos y métodos de otra, además de agregar de nuevos o modificar los heredados.
 
+#Clase Madre o SuperClase
 class Producto:
     #Los atributos con None hace referencia de que nos son parametros obligatorio
-    def __init__(self,referencia,tipo,nombre,pvp,descripcion,productor = None, dsitribuidor = None, isbmn = None, autor = None):
+    def __init__(self,referencia,nombre,pvp,descripcion):
         self.referencia = referencia
-        self.tipo = tipo
         self.nombre = nombre
         self.pvp = pvp
         self.descripcion = descripcion
-        self.productor = productor
-        self.dsitribuidor = dsitribuidor
-        self.isbn = isbmn
-        self.autor = autor
+    def __str__(self):
+        return """\
+        REFERENCIA\t{}
+        NOMBRE\t\t{}
+        PVP\t\t{}
+        DESCRIPCION\t{}""".format(self.referencia, self.nombre, self.pvp, self.descripcion)
     
-adorno = Producto('000A','ADORNO','Vaso Adornado',15,'Vaso de porcelana con dibujtos')
-print(adorno.tipo)
 
+#Clase Hija de producto
+class Adorno(Producto):
+    pass
+
+#Clase Hija de producto
+class Alimento(Producto):
+    productor = ""
+    distribuidor = ""
+
+    def __str__(self):
+        return """\
+        REFERENCIA\t{}
+        NOMBRE\t\t{}
+        PVP\t\t{}
+        DESCRIPCION\t{}
+        PRODUCTOR\t{}
+        DISTRIBUIDOR\t{}""".format(self.referencia, self.nombre, self.pvp, self.descripcion, self.productor, self.distribuidor)
+
+#Clase Hija de producto
+class Libro(Producto):
+    isbn = ""
+    autor = ""
+
+    def __str__(self):
+        return """\
+        REFERENCIA\t{}
+        NOMBRE\t\t{}
+        PVP\t\t{}
+        DESCRIPCION\t{}
+        ISBN\t\t{}
+        AUTOR\t\t{}""".format(self.referencia, self.nombre, self.pvp, self.descripcion, self.isbn, self.autor)
+
+
+#Objeto de la clase adorno
+a = Adorno(2034,"Vaso Adornado",15, "Vaso de porcelana adornado con arboles")
+#print(a)
+
+#Objeto de la clase alimento
+al = Alimento(2035,"Botellade Aceite",5,"250 ML")
+al.productor = "La Aceitera"
+al.distribuidor = "Distribuiciones SA"
+print("Clase hija Alimento")
+print(al)
+
+#Objeto de la clase libroo
+li =Libro(2036,"Cocina Mediterranea",9,"Recetas Sanas y buenas")
+li.isbn = "0-123456-78-9"
+li.autor = "Doña Juana"
+print("Clase hija Libro")
+print(li)
