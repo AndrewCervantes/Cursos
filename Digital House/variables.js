472c5c8456eed52790369fffc66df393eb835a7d
AndrewCervantes <andrez.cris10@gmail.com>
2021-02-24 23:36:31 -0600

    Introducci{on a la programación

---
+++ Digital House/variables.js
@@ -0,0 +1,20 @@
+// Variable .- Es un espacio de memoria donde se almacena un dato que podemos reutilizar a futuro.
+// Estandar de lenguaje - Es la manera adecuada de escribir código.
+
+//Camel Case
+var telefonoMadre
+
+//Snake Case
+var telefono_madre
+
+
+/* let vs var
+Let es una variable que pertenece a un bloque de ejecución {} 
+Una variable let no puede ser redefinida en el mismo bloque de ejecuci{on pero si puede vambiar su valor.
+*/
+
+//Bloque de ejecución
+if(true){
+    let saludo = 'hola';
+    console.log(saludo)
+}
