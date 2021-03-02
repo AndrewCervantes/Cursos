066763e472361add5b378a9cb020a35f467171dc
AndrewCervantes <andrez.cris10@gmail.com>
2021-03-01 23:08:22 -0600

    Condicionales

--- Digital House/condicionales.js
+++ Digital House/condicionales.js
@@ -12,3 +12,46 @@ if (clima == 'Soleado' && dia == 'domingo'){
     console.log("Mejor me quedo en casa");
 }
 
+//Se puede simplificar el if /else
+function esMayorDeEdad(edad) {
+    if ( edad >=  18){
+        return true;
+    } else {
+        return false;
+    }
+ }
+
+ //Equivale 
+ function esMayorDeEdad(edad) {
+    return edad >=  18;
+}
+
+function estaEntre(numero, base, limite) {
+    return numero > base  && numero < limite ;
+}
+
+function esMayorOEsMenor(numero1, numero2, numero3) {
+    return numero2 > numero1 || numero2 < numero3;
+}
+
+function esNumeroDeLaSuerte(numero){
+    return ((numero > 0)  && (numero %2==0 || numero %3==0) && (numero != 15));
+}
+
+console.log(esNumeroDeLaSuerte(6));
+
+/*
+En el parque de diversiones de la ciudad instalaron una nueva montaña rusa y nos pidieron ayuda para que le avisemos a las personas, si pueden subirse o no, antes de hacer la fila. Los requisitos para que una persona pueda subir a la atracción son:
+
+Alcanzar la altura mínima de 1.5m (o 1.2m si está acompañada por un adulto)
+No tener ninguna afección cardíaca
+
+*/
+
+function puedeSubirse(alturaPersona, vieneConCompania, tieneAfeccionCardiaca){
+    return permiso(alturaPersona, vieneConCompania) && !tieneAfeccionCardiaca;
+}
+
+function permiso(alturaPersona, vieneConCompania){
+    return alturaPersona >= 1.5 || (alturaPersona >= 1.2 && vieneConCompania)
+}
