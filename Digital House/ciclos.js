d7dbdc216c5bea197cdd0401c62378fb0d1954b9
AndrewCervantes <andrez.cris10@gmail.com>
2021-03-02 23:34:36 -0600

    Ciclos

--- Digital House/ciclos.js
+++ Digital House/ciclos.js
@@ -8,3 +8,20 @@ for (let i = 1; i <=10; i++){
     console.log(i * 7);
 
 }
+
+
+
+function pasandoPorI(){
+    for (let i = 0; i < 5; i++){
+        console.log("acá i tiene el valor de "+i);
+    }
+}
+/*Realizar una función llamada pasandoPorLosPares() que muestre por consola acá i tiene el valor de x por cada valor PAR que va tomando i desde 0 hasta 6 (inclusive). Te recordamos que debes prestar especial atención con los espacios. En el ejercicio debe haber un solo espacio entre cada palabra.*/
+function pasandoPorLosPares(){
+    for (let i = 0; i < 7; i++){
+        if (i %2 == 0){
+            console.log("acá i tiene el valor de "+i);
+        }
+    }
+}
+
