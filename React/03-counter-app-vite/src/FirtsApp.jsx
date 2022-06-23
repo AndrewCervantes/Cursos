26fa20ecc9251010863800946601859867b52a8c
Christian Cervantes <andrez.cris10@gmail.com>
2022-06-22 20:25:26 -0500

    Curso React - Fragmento

--- React/03-counter-app-vite/src/FirtsApp.jsx
+++ React/03-counter-app-vite/src/FirtsApp.jsx
@@ -1,5 +1,27 @@
+/* Cuando se desea regresar un grupo de elementos en functional componente debemos de enviar la etiqueta fragmente */
+
+/* Forma 1
+import { Fragment } from "react"
 export const FirtsApp = () => {
   return (
-    <div>FirtsApp - Christian Cervantes</div>
+    <Fragment>
+      <h1>FirtsApp - Christian Cervantes</h1>
+      <h2>Hola Mundo soy un subtitulo</h2>
+    </Fragment>
   )
 }
+
+*/
+
+/* Forma 2   <> - nodo padre*/
+export const FirtsApp = () => {
+  return (
+    <>
+      <h1>FirtsApp - Christian Cervantes</h1>
+      <h2>Hola Mundo soy un subtitulo</h2>
+    </>
+  )
+}
+
+
+
