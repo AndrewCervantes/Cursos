8ed09a20624eadab494fb70c87dea82d35857f68
Christian Cervantes <andrez.cris10@gmail.com>
2022-07-04 22:52:21 -0500

    Curso React - Props

--- React/03-counter-app-vite/src/FirstApp_Props.jsx
+++ React/03-counter-app-vite/src/FirstApp_Props.jsx
@@ -1,13 +1,28 @@
 /* Comunicación entre compomponentes - Props
   Propiedades que se mandan en un functional component.
 */
+
+/* Forma 1 - no recomendable 
 export const FirtsApp2 = (props) => {
-  console.log(props)
+  //console.log(props)
   return (
     <>
       <h1> Chrisitian </h1>
       <h2> {props.title} </h2>
+      {}
       <p>Soy un subtitulo</p>
+    </>
+  )
+}
+
+*/
+export const FirtsApp2 = ({title, subTitle}) =>{
+  return(
+    <>
+      <h1> Christian</h1>
+      <h2>{title}</h2>
+      <p>{subTitle + 10}</p>
+      <p>soy un subtitulo</p>
     </>
   )
 }
