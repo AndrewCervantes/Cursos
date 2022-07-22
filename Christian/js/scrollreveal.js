8ee5a1df986a1ae1e65ffa3411166ea57a6f9631
Christian Cervantes <andrez.cris10@gmail.com>
2022-07-21 21:07:31 -0500

    Add Experience

---
+++ Christian/js/scrollreveal.js
@@ -0,0 +1,27 @@
+window.sr =  ScrollReveal();
+
+/* Header */
+sr.reveal('.header',{
+    duration: 4000,
+    origin: 'buttom',
+    distance: '-2rem'
+});
+
+/* About */
+sr.reveal('.scroll-banner-left',{
+    duration: 4000,
+    origin: 'left',
+    distance: '-2rem'
+});
+sr.reveal('.scroll-banner-right',{
+    duration: 4000,
+    origin: 'right',
+    distance: '-2rem'
+});
+
+/* Experience */
+sr.reveal('.company',{
+    duration: 4000,
+    origin: 'buttom',
+    distance: '-3rem'
+});
